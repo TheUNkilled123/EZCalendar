0139816e21230d0bb322267d3892c7cb76a349b2
Toni <toni.jelasac@gmail.com>
2021-06-01 21:00:33 +0200

    Implemented Google FireStore and date selection with experimental task uploading.

--- app/src/main/java/com/example/ezcalendar/MainActivity.java
+++ app/src/main/java/com/example/ezcalendar/MainActivity.java
@@ -1,14 +1,154 @@
 package com.example.ezcalendar;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.CalendarView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreSettings;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    Calendar cal = Calendar.getInstance();
+    Calendar cal1 = Calendar.getInstance();
+    Date newDate = cal.getTime();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        FirebaseFirestoreSettings settings = new FirebaseFirestoreSettings.Builder()
+                .setPersistenceEnabled(false)
+                .build();
+        db.setFirestoreSettings(settings);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        CalendarView calView = findViewById(R.id.calendarView);
+        ExtendedFloatingActionButton baton = findViewById(R.id.addEvent);
+
+        //Fires when date changed in the calendar
+        calView.setOnDateChangeListener(new CalendarView.OnDateChangeListener(){
+
+            public void onSelectedDayChange(CalendarView view, int year, int month, int dayOfMonth) {
+                cal.set(year, month, dayOfMonth);
+                newDate = cal.getTime();
+            }});
+
+
+        baton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Map<String, Object> event = new HashMap<>();
+                event.put("date", newDate);
+                event.put("eventTitle", "Zubar");
+                event.put("eventDesc", "Kao nesto moras kod zubara ali samo ako te boli zub ili nesto test droime skaldjasdjaksjdkasjd.");
+
+                db.collection("events")
+                        .add(event)
+                        .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                            @Override
+                            public void onSuccess(DocumentReference documentReference) {
+                                Log.d("E", "DocumentSnapshot added with ID: " + documentReference.getId());
+                            }
+                        })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Log.w("E", "Error adding document", e);
+                            }
+                        });
+
+                cal.set(Calendar.SECOND, 1);
+                cal.set(Calendar.MILLISECOND, 0);
+                cal.set(Calendar.HOUR_OF_DAY, 0);
+                cal.set(Calendar.MINUTE, 0);
+
+                cal1.setTime(cal.getTime());
+                cal1.add(cal1.DATE,1); Log.d("TESTINGMOMSAODMOASMDOASMDOASMD", String.valueOf(cal1.getTime())); Log.d("TESTINGMOMSAODMOASMDOASMDOASMD", String.valueOf(cal.getTime()));
+
+                db.collection("events")
+                        .whereLessThan("date",cal1.getTime())
+                        .whereGreaterThan("date",cal.getTime())
+                        .get()
+                        .addOnCompleteListener(task -> {
+                            if (task.isSuccessful()) {
+                                for (QueryDocumentSnapshot document : task.getResult()) {
+                                    Log.d("OUTPUT", document.getId() + " => " + document.getData());
+                                }
+                            } else {
+                                Log.w("OUTPUT", "Error getting documents.", task.getException());
+                            }
+                        });
+
+            }
+        });
+
+
+
+
     }
 }
+
+
+//Calendar.getInstance().set(year, month, day, hour, minutes, seconds);
+
+
+// Saving data
+/*
+Map<String, Object> event = new HashMap<>();
+        event.put("date", "10/06/2021");
+                event.put("eventTitle", "Zubar");
+                event.put("eventDesc", "Kao nesto moras kod zubara ali samo ako te boli zub ili nesto test droime skaldjasdjaksjdkasjd.");
+
+
+                db.collection("events")
+                .add(event)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+@Override
+public void onSuccess(DocumentReference documentReference) {
+        Log.d("E", "DocumentSnapshot added with ID: " + documentReference.getId());
+        }
+        })
+        .addOnFailureListener(new OnFailureListener() {
+@Override
+public void onFailure(@NonNull Exception e) {
+        Log.w("E", "Error adding document", e);
+        }
+        });
+
+ */
+
+//Reading data
+
+/*
+db.collection("events")
+                .get()
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        for (QueryDocumentSnapshot document : task.getResult()) {
+                            Log.d("OUTPUT", document.getId() + " => " + document.getData());
+                        }
+                    } else {
+                        Log.w("OUTPUT", "Error getting documents.", task.getException());
+                    }
+                });
+*/
